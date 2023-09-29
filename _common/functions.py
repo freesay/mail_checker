249e4e1f143aa15fa51d550eb243c5dbf3182d94
freesay <freesay@inbox.ru>
2023-09-29 10:41:52 +0500

    Update functions.py

--- _common/functions.py
+++ _common/functions.py
@@ -1,14 +1,18 @@
+import os
 from pathlib import Path
 from _common import parser
 from _common import generators as gen
 
 
 def _get_data(file):
-    root_path = Path(__file__).parents[1]
-    file_path = Path(root_path, file)
-    with open(file_path, 'r', encoding='utf-8-sig') as f:
-        data = f.read()
-    return data
+    try:
+        root_path = Path(__file__).parents[1]
+        file_path = Path(root_path, file)
+        with open(file_path, 'r', encoding='utf-8-sig') as f:
+            data = f.read()
+        return data
+    except:
+        return 'There is no data of this type.'
 
 
 class Functions:
@@ -16,13 +20,23 @@ class Functions:
         self.interface = interface
 
     def open_file(self):
-        file = self.interface.file_dialog()
-        self.data = parser.get_raw_data(file)
-        self.set_data_text_box(self.data)
-        self.update_detect_text(f'Open file: {file}')
+        self.file = self.interface.file_dialog()
+        self.raw_data = parser.get_raw_data(self.file)
+        self.set_data_text_box(self.raw_data)
+        self.interface.frame_text_box.config(text=self.file)
 
-    def update_detect_text(self, detect_text):
-        self.interface.detect_label.config(text=detect_text)
+    def open_folder(self):
+        root_path = Path(__file__).parents[1]
+        attaches_folder = (Path(root_path, 'attaches'))
+        os.system(f'start {attaches_folder}')
+
+    def open_clear_html(self):
+        root_path = Path(__file__).parents[1]
+        attaches_folder = str(Path(root_path, 'resources', 'clear_html.html'))
+        os.system(fr'{attaches_folder}')
+
+    def update_detect_text(self, detect_file):
+        self.interface.detect_label.config(text=detect_file)
 
     def set_data_text_box(self, data):
         self.interface.text_box.configure(state='normal')
@@ -32,39 +46,27 @@ class Functions:
 
     def get_convert_data(self):
         gen.clear_folders()
-        parser.parse_content(self.data)
+        parser.parse_content(self.raw_data)
         data = gen.get_folder_content()
-        self.update_detect_text(f'Attached files were found in the email: {data}')
+        self.interface.create_files_widget(data)
+        self.interface.create_open_html_widget()
 
     def set_headers_tex_box(self):
         location_file = Path('resources', 'res_headers.txt')
-        try:
-            data = _get_data(location_file)
-            self.set_data_text_box(data)
-        except:
-            self.update_detect_text('There is no content of this type')
+        data = _get_data(location_file)
+        self.set_data_text_box(data)
 
     def set_plain_tex_box(self):
         location_file = Path('resources', 'res_plain.txt')
-        try:
-            data = _get_data(location_file)
-            self.set_data_text_box(data)
-        except:
-            self.update_detect_text('There is no content of this type')
-
+        data = _get_data(location_file)
+        self.set_data_text_box(data)
 
     def set_html_tex_box(self):
         location_file = Path('resources', 'res_html.html')
-        try:
-            data = _get_data(location_file)
-            self.set_data_text_box(data)
-        except:
-            self.update_detect_text('There is no content of this type')
+        data = _get_data(location_file)
+        self.set_data_text_box(data)
 
     def set_urls_tex_box(self):
         location_file = Path('resources', 'res_urls.txt')
-        try:
-            data = _get_data(location_file)
-            self.set_data_text_box(data)
-        except:
-            self.update_detect_text('There is no content of this type')
+        data = _get_data(location_file)
+        self.set_data_text_box(data)
