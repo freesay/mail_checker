c17c8f05b0cfe7017e9918d75474115c5ba03a03
freesay <freesay@inbox.ru>
2024-03-04 05:54:34 +0500

    Create check_pdf.py

---
+++ _common/check_pdf.py
@@ -0,0 +1,18 @@
+import tst_pdfid
+
+
+def analyze_pdfs_by_filenames(filenames):
+    options = tst_pdfid.get_fake_options()
+    options.scan = True
+    options.json = True
+    list_of_dict = tst_pdfid.PDFiDMain(filenames, options)
+    return list_of_dict
+
+
+def get_report(filename):
+    filenames = [filename]
+    res_filenames = analyze_pdfs_by_filenames(filenames)['reports'][0]
+    res = ''
+    for el, val in res_filenames.items():
+        res += f'{el.ljust(20, "_")} {val}\n'
+    return res
